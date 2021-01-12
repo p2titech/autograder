a6c3f1d3c984a84e792666a5153f23d78e8a0b21
Yusuke Izawa <yuizalp@gmail.com>
2021-01-12 16:00:17 +0900

    Fixed sync.sh

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -3,9 +3,11 @@
 for repo in r1 r1p2 r2p1 r2p2 r2p3 r3p1 r3p2
 do
   cp runtest.sh assignments/"${repo}"
-  cd assignments/"${repo}" || exit
+  pushd assignments/"${repo}" || exit
   git switch master || git switch main
+  git pull
   git add runtest.sh
   git commit -m "Updated runtest.sh"
   git push
+  popd
 done
