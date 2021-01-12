edb69e727018eab08bcaa54ac7f36f67c733298f
Yusuke Izawa <yuizalp@gmail.com>
2021-01-12 15:54:29 +0900

    Updated sync.sh

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-for repo in r1 r1p2 r2p1 r2p2 r2p3
+for repo in r1 r1p2 r2p1 r2p2 r2p3 r3p1 r3p2
 do
-  cp runtest.sh ../"${repo}"
-  cd ../"${repo}" || exit
+  cp runtest.sh assignments/"${repo}"
+  cd assignments/"${repo}" || exit
   git switch master || git switch main
   git add runtest.sh
   git commit -m "Updated runtest.sh"
