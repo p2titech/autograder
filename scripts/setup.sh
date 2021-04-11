3465d0bb35484262dd8d1f859ae10d0be51ae8b9
Yusuke Izawa <yuizalp@gmail.com>
2021-04-12 00:56:31 +0900

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for assignment in r1 r1p2 r2p1 r2p2 r2p3 r3p1 r3p2; do
+for assignment in r1 r1p2 r2p1 r2p2 r2p3 r3p1 r3p2 r4p1 r5p1 r5p2 p6p1 r7p1; do
   if [[ ! -d assignments/"$assignment" ]]; then
     gh repo clone p2titech/"$assignment" assignments/"$assignment"
   fi
